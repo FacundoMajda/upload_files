085148716686020da8fcc7e43877e39633af16fd
FacundoMajda <facundomajda13@gmail.com>
2023-08-09 12:38:38 -0300

    dependencies, config app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const ejs = require("ejs");
+const mysql = require("mysql2");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(fileUpload());
+
+// Configurar EJS como motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
