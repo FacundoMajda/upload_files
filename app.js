a0a6f85b4fbbfb412159f987757675fc793edd90
FacundoMajda <facundomajda13@gmail.com>
2023-08-10 18:49:58 -0300

    db connect

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ const mysql = require("mysql2");
 const { Sequelize, DataTypes } = require("sequelize");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+
+//variables de entorno
+dotenv.config({ path: ".env" });
+
+// Se importa la instancia de conexión a la base de datos
+const { sequelize } = require("./db.js");
+
+// Configurar EJS como motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middlewares
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 
@@ -28,9 +38,23 @@ app.use(
   })
 );
 
-// Configurar EJS como motor de plantillas
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// default options
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Se ejecuta una instancia de conexión a la base de datos
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a base de datos exitosa");
+  })
+  .catch((error) => console.log("Error al conectar a base de datos", error));
+
+// app.use("/", require("./routes/galleries.routes"));
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
