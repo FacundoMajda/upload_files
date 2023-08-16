e9b173791b43c21769671acf60dd559b399cb007
FacundoMajda <facundomajda13@gmail.com>
2023-08-16 11:54:42 -0300

    keep refact

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const fileUpload = require("express-fileupload");
@@ -11,13 +12,21 @@ const mysql = require("mysql2");
 const { Sequelize, DataTypes } = require("sequelize");
 const createError = require("http-errors");
 
-//environment
-dotenv.config({ path: ".env" });
+//asd
+const signuploadRouter = require("./routes/signupload");
+const signuploadformRouter = require("./routes/signuploadform");
+
 const app = express();
 const port = process.env.PORT || 4000;
 
 const { sequelize } = require("./db.js");
 
+// upload signing API
+app.use("/api/signupload", signuploadRouter);
+app.use("/api/signuploadform", signuploadformRouter);
+
+// static files
+app.use(express.static("public"));
 // // Configurar EJS como motor de plantillas
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
@@ -26,19 +35,16 @@ const { sequelize } = require("./db.js");
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-
-// static files
-app.use(express.static("public"));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "public")));
-
-// static files
-app.use(express.static("public"));
-
 app.use(fileUpload());
+// default options
+app.use(
+  fileUpload({
+    useTempFiles: false,
+  })
+);
 
 app.use(
   helmet({
@@ -51,23 +57,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// default options
-app.use(
-  fileUpload({
-    useTempFiles: false,
-  })
-);
-
 // app.use("/", require("./routes/file.routes"));
-
-// upload signing API
-app.use("/api/signupload", signuploadRouter);
-app.use("/api/signuploadform", signuploadformRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function (err, req, res, next) {
